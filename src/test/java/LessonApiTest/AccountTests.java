cf9d5206faafb8339d8e6c1c1b00d726cb443ea2
Chaika <chaikaes@gmail.com>
2021-12-11 21:11:42 +0300

    Refactor account tests

--- src/test/java/LessonApiTest/AccountTests.java
+++ src/test/java/LessonApiTest/AccountTests.java
@@ -1,66 +1,42 @@
 package LessonApiTest;
 
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import io.restassured.response.Response;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class AccountTests extends BaseTest{
 
     @Test
     void getAccountInfoTest() {
-        given()
-                .header("Authorization", token)
-                .when()
+        given(requestWithAuth)
                 .get("https://api.imgur.com/3/account/{username}", username)
-                .then()
-                .statusCode(200);
-    }
-
-    @Test
-    void getAccountInfoWithLoggingTest() {
-        given()
-                .header("Authorization", token)
-                .log()
-                .method()
-                .log()
-                .uri()
-                .when()
-                .get("https://api.imgur.com/3/account/{username}", username)
-                .prettyPeek()
-                .then()
-                .statusCode(200);
+                .prettyPeek();
     }
 
     @Test
     void getAccountInfoWithAssertionsInGivenTest() {
-        given()
-                .header("Authorization", token)
-                .log()
-                .method()
-                .log()
-                .uri()
-                .expect()
-                .statusCode(200)
-                .body("data.url", equalTo(username))
-                .body("success", equalTo(true))
-                .body("status", equalTo(200))
-                .contentType("application/json")
-                .when()
+        positiveResponse = new ResponseSpecBuilder()
+                .expectBody("data.url", equalTo(username))
+                .expectBody("status", Matchers.equalTo(200))
+                .expectBody("success", is(true))
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .build();
+
+        given(requestWithAuth, positiveResponse)
                 .get("https://api.imgur.com/3/account/{username}", username)
                 .prettyPeek();
     }
 
     @Test
     void getAccountInfoWithAssertionsAfterTest() {
-        Response response = given()
-                .header("Authorization", token)
-                .log()
-                .method()
-                .log()
-                .uri()
-                .when()
+        Response response = given(requestWithAuth)
                 .get("https://api.imgur.com/3/account/{username}", username)
                 .prettyPeek();
 
